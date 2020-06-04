31d4d03c95cbb05670748973b8f194665734717d
cosmic-tichy <nbmowery@iu.edu>
2020-06-04 18:39:46 -0400

    Added vuex for state management and removed vue2-leaflet

--- frontend/src/components/MapTools.vue
+++ frontend/src/components/MapTools.vue
@@ -1,14 +1,35 @@
 <template>
-<div>
-     <b-form-group label="Using sub-components:">
-      <b-form-checkbox-group id="checkbox-group-2" v-model="selected" name="flavour-2">
-        <b-form-checkbox value="ucerf" v-on:change="layers[0].active=!(layers[0].active)"><p>UCERF3 Faults</p></b-form-checkbox>
-        <b-form-checkbox value="kml"><p>KML Mapper</p></b-form-checkbox>
-        <b-form-checkbox value="states"><p>Show State Boundaries</p></b-form-checkbox>
-        <b-form-checkbox value="coasts"><p>Show Coastlines</p></b-form-checkbox>
-      </b-form-checkbox-group>
-    </b-form-group>
-</div>
+    <div>
+        <b-form-group >
+            <b-form-checkbox-group id="checkbox-group-2" v-model="selected" name="flavour-2" stacked >
+                <b-form-checkbox value="ucerf" v-on:change="mapTools.ucerf=!mapTools.ucerf"><p>UCERF3 Faults</p></b-form-checkbox>
+                <b-form-checkbox value="kml" v-on:change="mapTools.kml=!mapTools.kml"><p>KML Mapper</p></b-form-checkbox>
+                <b-form-checkbox value="states" v-on:change="mapTools.boundaries=!mapTools.boundaries"><p>Show State Boundaries</p></b-form-checkbox>
+                <b-form-checkbox value="coasts" v-on:change="mapTools.coasts=!mapTools.coasts"><p>Show Coastlines</p></b-form-checkbox>
+            </b-form-checkbox-group>
+        </b-form-group>
+
+        <div id="tools-show">
+            <div v-if="mapTools.ucerf">
+                <hr class="divider" />
+                <b-form-radio-group  >
+                    <b-form-radio v-model="selected" name="some-radios" value="black"><p>black</p></b-form-radio>
+                    <b-form-radio v-model="selected" name="some-radios" value="red"><p>red</p></b-form-radio>
+                    <b-form-radio v-model="selected" name="some-radios" value="yellow"><p>yellow</p></b-form-radio>
+                    <b-form-radio v-model="selected" name="some-radios" value="grey"><p>grey</p></b-form-radio>
+                </b-form-radio-group>
+            </div>
+
+            <div v-if="mapTools.kml">
+                <hr class="divider" />
+                <h2>KML File Upload</h2>
+                <form method="post" enctype="multipart/form-data">
+                    <input type="file" name="kml-file"> <br/><br/>
+                    <button type="submit">Upload File</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -23,8 +44,8 @@
 
         },
         computed: {
-            layers(){
-                return this.$store.state.layers;
+            mapTools(){
+                return this.$store.state.mapTools;
             }
         }
 
@@ -32,7 +53,15 @@
 </script>
 
 <style scoped>
- p {
-     color: #cccccc;
- }
+    p, h1, h2, h3, h4, hr {
+        color: #cccccc;
+    }
+    #tools-show {
+        height: 100%;
+    }
+    .divider {
+         border: 2px solid #cccccc;
+        border-radius: 1px;
+    }
+
 </style>
--- frontend/src/components/MyMap.vue
+++ frontend/src/components/MyMap.vue
@@ -1,77 +1,111 @@
 <template>
-
-       <div id="map">
-    </div>
+        <div id="map-window">
+                <div id="map">
+                </div>
+                <b-button id="refreshButton" v-on:click="refreshLayers()">Refresh</b-button>
+        </div>
 
 
 </template>
 
 <script>
+        import L from 'leaflet';
+        import 'leaflet/dist/leaflet.css';
+        import "leaflet-kml"
+        export default {
+                name: 'MyMap',
+                components: {
+                },
+                data() {
+                        return {
+                                ucerfLayer: null,
+                                boundariesLayer: null,
+                                coastsLayer: null,
+                                map: null,
+                        };
+                },
+                mounted() {
+                        //create layers
+                        this.map = L.map('map').setView([51.505, -0.09], 3);
+                        L.tileLayer(
+                                'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
+                                {
+                                        maxZoom: 18,
+                                        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+                                }).addTo(this.map);
 
+                },
 
-    import L from 'leaflet';
-    import "../assets/leaflet-kml"
-    export default {
-        name: 'MyMap',
-        components: {
-        },
-        mounted() {
-            this.initMap();
-        },
+                methods: {
+                        refreshLayers(){
+                                if (this.$store.state.mapTools.ucerf) {
+                                        this.ucerfLoad();
+                                }else {this.map.removeLayer(this.ucerfLayer);}
 
-        computed: {
-            ucerfLayer(){
-                return this.$store.state.layers[0].active;
-            }
-        },
-        methods: {
-            initMap(){
-                var mymap = L.map('map').setView([51.505, -0.09], 3);
+                                if (this.$store.state.mapTools.boundaries) {
+                                        this.boundariesLoad();
+                                }else {this.map.removeLayer(this.boundariesLayer);}
 
-                this.tileLayer = L.tileLayer(
-                    'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
-                    {
-                        maxZoom: 18,
-                        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
-                    }
-                );
-                this.tileLayer.addTo(mymap);
-            },
+                                if (this.$store.state.mapTools.coasts) {
+                                        this.coastsLoad();
+                                }else {this.map.removeLayer(this.coastsLayer);}
+                        },
+                        ucerfLoad() {
+                                fetch('https://raw.githubusercontent.com/GeoGateway/GeoGatewayStaticResources/master/kmz/ucerf3_black.kml').then(res => res.text())
+                                        .then(kmltext => {
+                                                const parser = new DOMParser();
+                                                var kml = parser.parseFromString(kmltext, "text/xml");
+                                                this.ucerfLayer = new L.KML(kml);
+                                                this.map.addLayer(this.ucerfLayer);
+                                                this.map.fitBounds(this.ucerfLayer.getBounds());
+                                        });
+                        },
+                        boundariesLoad() {
+                                fetch('https://raw.githubusercontent.com/GeoGateway/GeoGatewayStaticResources/master/kmz/gz_2010_us_040_00_20m.kml').then(res => res.text())
+                                        .then(kmltext => {
+                                                const parser = new DOMParser();
+                                                var kml = parser.parseFromString(kmltext, "text/xml");
+                                                this.boundariesLayer = new L.KML(kml);
+                                                this.map.addLayer(this.boundariesLayer);
+                                                this.map.fitBounds(this.boundariesLayer.getBounds());
+                                        });
+                        },
+                        coastsLoad() {
+                                fetch('https://raw.githubusercontent.com/GeoGateway/GeoGatewayStaticResources/master/kmz/ne_50m_coastline.kml').then(res => res.text())
+                                        .then(kmltext => {
+                                                const parser = new DOMParser();
+                                                var kml = parser.parseFromString(kmltext, "text/xml");
+                                                this.coastsLayer = new L.KML(kml);
+                                                this.map.addLayer(this.coastsLayer);
+                                                this.map.fitBounds(this.coastsLayer.getBounds());
+                                        });
+                        }
+                },
 
-            ucerfShow(map) {
-                fetch('https://raw.githubusercontent.com/GeoGateway/GeoGatewayStaticResources/master/kmz/ucerf3_black.kml')
-                    .then(res => res.text())
-                    .then(kmltext => {
-                        // Create new kml overlay
-                        const parser = new DOMParser();
-                        const kml = parser.parseFromString(kmltext, 'text/xml');
-                        const track = new L.KML(kml);
-                        map.addLayer(track);
-
-                        // Adjust map to show the kml
-                        const bounds = track.getBounds();
-                        map.fitBounds(bounds);
-                    });
-            }
-        },
-
-    };
+        };
 </script>
 <style scoped>
-    #map {
-        position: relative;
-        height: 96%;
-        width: auto;
-        margin-left: auto;
-        padding: 0px;
-        /*float: right;*/
+        #map {
+                position: relative;
+                height: 96%;
+                width: auto;
+                margin-left: auto;
+                padding: 0px;
+                /*float: right;*/
 
-    }
-    #map-window {
-        position: relative;
-        height: 100%;
-        width: auto;
-        margin-left: auto;
-        padding: 0px;
-    }
+        }
+        #map-window {
+                position: relative;
+                height: 100%;
+                width: calc(100% - 400px);
+                margin-left: auto;
+                padding: 0px;
+        }
+        #refreshButton {
+                position: absolute;
+                top: 20px;
+                right: 20px;
+                padding: 10px;
+                z-index: 400;
+        }
 </style>
