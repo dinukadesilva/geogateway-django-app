923e874863317c88657449f0358b102e524bd4dc
webgisdeveloper <wang208@indiana.edu>
2021-03-09 21:51:39 -0500

    #24 update help page

--- frontend/src/components/help.vue
+++ frontend/src/components/help.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
         <h3>User Guide</h3>
+      <!--  
       <a href="https://archive.geo-gateway.org:3000/main.html" target="_blank"> <i>Visit the archived Geo-Gateway</i></a>
+     -->
+      <div class="tool-content">
+      <a target="_blank" href="https://github.com/GeoGateway/GeoGatewayStaticResources/raw/master/documents/GeoGateway_User_Guide.pdf"><strong>User Guide (pdf)</strong></a>
+      <br>Lastest user guide will be posted soon.
+      <br><br>
+      <a target="_blank" href="https://youtu.be/Dz2JPgCpd-k"><strong>GSA 2020 Workshop (video)</strong></a>
+      <br><br>
+      <a target="_blank" href="https://github.com/GeoGateway/GeoGatewayStaticResources/raw/master/documents/GeoGateway_SSA_Workshop.pdf"><strong>SSA 2019 GeoGateway Workshop (slide)</strong></a>
+      </div>
     </div>
 </template>
 
