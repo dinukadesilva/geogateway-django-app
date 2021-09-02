7b9999dc22a935d9ba54ad3556f77a84197d67db
webgisdeveloper <wang208@indiana.edu>
2021-09-02 13:12:55 -0400

    exercise enabled

--- frontend/src/components/help.vue
+++ frontend/src/components/help.vue
@@ -6,10 +6,10 @@
      -->
       <div class="tool-content">
       <a target="_blank" href="https://github.com/GeoGateway/GeoGatewayStaticResources/raw/master/documents/GeoGateway_User_Guide.pdf"><strong>User Guide (pdf)</strong></a>
-      <br>(Work in Progress, Corrections coming soon)<br>
-      <!--
+      <br>(Work in Progress)<br>
+     
      <a target="_blank" href="https://github.com/GeoGateway/GeoGatewayStaticResources/raw/master/documents/GeoGateway_Exercises.pdf"><strong>Exercises (pdf)</strong></a>
-       <br> --> <br>
+       <br> <br>
       <a target="_blank" href="https://earthdata.nasa.gov/learn/articles/getting-started-with-sar"><strong>GSA 2020 Workshop</strong></a>
       <br/><a target="_blank" href="https://cdn.earthdata.nasa.gov/conduit/upload/15244/GSA__2020_GeoGateway_updated_October_14.pdf"><strong>GSA 2020 Workshop (slides)</strong></a>
       <br/><a target="_blank" href="https://youtu.be/Dz2JPgCpd-k"><strong>GSA 2020 Workshop (video)</strong></a>
