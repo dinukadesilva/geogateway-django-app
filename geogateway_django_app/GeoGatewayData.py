3978ff0a7c8df1f5c4f75d57da145fef6e70170b
webgisdeveloper <wang208@indiana.edu>
2022-01-19 12:28:05 -0500

    #189 los update

--- geogateway_django_app/GeoGatewayData.py
+++ geogateway_django_app/GeoGatewayData.py
@@ -20,7 +20,7 @@ KmlPrefix = "https://archive.geo-gateway.org/static"
 #wmsUrl = 'http://js-168-95.jetstream-cloud.org/geoserver/highres/wms?'
 wmsColorUrl = 'https://archive.geo-gateway.org/color/InSAR/wms?'
 wmsUrl = 'https://archive.geo-gateway.org/color/highres/wms?'
-losQueryUrl = 'http://js-170-143.jetstream-cloud.org:8000/los/profile?dem=True&image=uid'
+losQueryUrl = 'https://archive.geo-gateway.org/los/profile?dem=True&image=uid'
 
 WoForecastUrl = 'http://www.openhazards.com/Tools/kml/wo-forecast.kmz'
 CaForecastUrl = 'http://www.openhazards.com/Tools/kml/ca-forecast.kmz'
