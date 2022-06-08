762d190a3c9887b37b71dfba8ac420ef8c31396e
Dinuka De Silva <l.dinukadesilva@gmail.com>
2022-06-07 23:35:14 -0400

    Adding sass and enable bootstrap theaming

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,44 +3,55 @@ const BundleTracker = require("webpack-bundle-tracker");
 module.exports = {
     publicPath:
         process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:9000/static/geogateway_django_app/bundles/": "/static/geogateway_django_app/bundles/",
+            ? "http://127.0.0.1:9000/static/geogateway_django_app/bundles/" : "/static/geogateway_django_app/bundles/",
     outputDir: '../geogateway_django_app/static/geogateway_django_app/bundles',
 
     devServer: {
         disableHostCheck: true
     },
-  configureWebpack: {
-    optimization: {
+    configureWebpack: {
+        optimization: {
 
-      splitChunks: {
-        cacheGroups: {
-          vendors: {
-            name: 'chunk-vendors',
-            test: /[\\/]node_modules[\\/]/,
-            priority: -10,
-            chunks: 'initial'
-          },
+            splitChunks: {
+                cacheGroups: {
+                    vendors: {
+                        name: 'chunk-vendors',
+                        test: /[\\/]node_modules[\\/]/,
+                        priority: -10,
+                        chunks: 'initial'
+                    },
+                }
+            }
         }
-      }
-    }
-  },
+    },
 
     chainWebpack: config => {
 
-config
-    .plugin('BundleTracker')
-    .use(BundleTracker, [{filename: 'webpack-stats.json', path: '../geogateway_django_app/static/geogateway_django_app/bundles/'}])
+        config.module
+            .rule('scss')
+            .test(/\.scss$/,)
+            .use('sass-loader')
+            .loader('css-loader')
+            .loader('vue-style-loader')
+            .end()
 
-config.resolve.alias
-    .set('__STATIC__', 'static')
+        config
+            .plugin('BundleTracker')
+            .use(BundleTracker, [{
+                filename: 'webpack-stats.json',
+                path: '../geogateway_django_app/static/geogateway_django_app/bundles/'
+            }])
 
-config.devServer
-    .public('http://0.0.0.0:9000')
-    .host('0.0.0.0')
-    .port(9000)
-    .hotOnly(true)
-    .watchOptions({poll: 1000})
-    .https(false)
-    .headers({"Access-Control-Allow-Origin": ["\*"]})
-}
+        config.resolve.alias
+            .set('__STATIC__', 'static')
+
+        config.devServer
+            .public('http://0.0.0.0:9000')
+            .host('0.0.0.0')
+            .port(9000)
+            .hotOnly(true)
+            .watchOptions({poll: 1000})
+            .https(false)
+            .headers({"Access-Control-Allow-Origin": ["\*"]})
+    }
 };
