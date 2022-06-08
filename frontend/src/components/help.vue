1d4f29a0da5f22c56792f249e6311444dc41600e
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2022-06-08 08:12:49 -0400

    #239 papers removed from help

--- frontend/src/components/help.vue
+++ frontend/src/components/help.vue
@@ -11,25 +11,17 @@
        <br/>
        <a target="_blank" href="https://github.com/GeoGateway/GeoGatewayStaticResources/raw/master/documents/GeoGateway_Exercises.pdf"><strong>Exercises (pdf)</strong></a>
        <br/><br/>
+    <!--
        <strong>Recent Publications</strong>
        <div align="left" style="width:300px;">
        <b-list-group>
-           <!-- <b-list-group-item>
-       <a target="_blank" href="https://doi.org/10.1007/s12145-020-00561-7">Donnellan, A., Parker, J., Heflin, M. et al. Improving access to geodetic imaging crustal deformation data using GeoGateway. Earth Science Informatics, (2021).</a> 
-           </b-list-group-item>
-           <b-list-group-item > 
-               <a target="_blank" href="https://doi.org/10.1029/2019EA000644">Heflin, M., Donnellan, A., Parker, J. et al. Automated Estimation and Tools to Extract Positions, Velocities, Breaks, and Seasonal Terms From Daily GNSS Measurements: Illuminating Nonlinear Salton Trough Deformation. Earth and Space Science, (2020).</a>
-           </b-list-group-item>          
-           <b-list-group-item > 
-               <a target="_blank" href="https://doi.org/10.1002/essoar.10506047.1">Parker, J., Bilham, R., Donnellan, A. et al. Buried Aseismic Slip and Off‐Fault Deformation on the Southernmost San Andreas Fault triggered by the 2010 El Mayor Cucapah Earthquake revealed by UAVSAR. Earth and Space Science, (2021).</a>
-           </b-list-group-item>           -->
            <b-list-group-item v-for="item in papers" :key="item.year">
                <a target="_blank" :href= "item.doiurl">{{ item.title }}</a>
             </b-list-group-item>
        </b-list-group>
        </div>
-       <br/>
-       <strong>More</strong><br/>
+       <br/> -->
+       <h3>More</h3>
       <a target="_blank" href="https://earthdata.nasa.gov/learn/articles/getting-started-with-sar"><strong>GSA 2020 Workshop</strong></a>
       <br/><a target="_blank" href="https://cdn.earthdata.nasa.gov/conduit/upload/15244/GSA__2020_GeoGateway_updated_October_14.pdf"><strong>GSA 2020 Workshop (slides)</strong></a>
       <br/><a target="_blank" href="https://youtu.be/Dz2JPgCpd-k"><strong>GSA 2020 Workshop (video)</strong></a>
