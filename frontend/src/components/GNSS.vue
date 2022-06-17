7b8f37d9424861b0608fcf6269334c1bc42ba82a
Dinuka De Silva <l.dinukadesilva@gmail.com>
2022-06-17 03:35:24 -0400

    Formatting

--- frontend/src/components/GNSS.vue
+++ frontend/src/components/GNSS.vue
@@ -6,57 +6,62 @@
       <i class="aboutIcon fas fa-info-circle"></i> 
     </span> &ensp; About GNSS data Analysis
       </b-card>
-      <span class="inputLabel">Outputs <hr class="sectionLine" /></span>
-   <br>
+      <span class="inputLabel">Outputs <hr class="sectionLine"/></span>
+      <br>
       <b-card>
         <div v-if="gnssLayers.length!==0 && !activeGnssQuery">
-            <strong>Output</strong>
-            <div  v-for="layer in gnssLayers" :key="layer.name">
-              <b-card v-if="layer.type !== 'table.txt'" >
-                <b-form-checkbox :value="layer.active" v-model="layer.active" @change="showHideLayers(layer.active, layer)"> 
-                  <span class="checkbox-label"> <a :href="layer.url">{{layer.pre}} {{layer.type}}</a> </span> 
-                </b-form-checkbox>
-                </b-card>
-              <div v-else ><a style="color:#EB9040;" target="_blank" :href="layer.url">{{layer.name}}</a></div>
-            </div>
+          <strong>Output</strong>
+          <div v-for="layer in gnssLayers" :key="layer.name">
+            <b-card v-if="layer.type !== 'table.txt'">
+              <b-form-checkbox :value="layer.active" v-model="layer.active"
+                               @change="showHideLayers(layer.active, layer)">
+                <span class="checkbox-label"> <a :href="layer.url">{{ layer.pre }} {{ layer.type }}</a> </span>
+              </b-form-checkbox>
+            </b-card>
+            <div v-else><a style="color:#EB9040;" target="_blank" :href="layer.url">{{ layer.name }}</a></div>
           </div>
-          <div v-else><span style="color:#95ABB1;">No models applied!</span></div>
-        </b-card>
-  
+        </div>
+        <div v-else><span style="color:#95ABB1;">No models applied!</span></div>
+      </b-card>
 
-      <span class="inputLabel">Functions <hr class="sectionLine" /></span>
+
+      <span class="inputLabel">Functions <hr class="sectionLine"/></span>
       <br>
 
       <span class="inputLabel">GNSS data models</span>
       <br>
 
-        <!-- <label for="sel1">Select list:</label> -->
-        <select  class="form-control" v-model="kmltype_sel" id="kmltype_sel" >
-        <option disabled value='null' >Select a GNSS data model</option>
-          <option value='getvelocities'>Velocities</option>
-          <option value='getcoseismic'>Coseismic</option>
-          <option value='getpostseismic'>Postseismic</option>
-          <option value='getdisplacement'>Displacement</option>
-          <option value='getmodel'>Model</option>
-        </select>
+      <!-- <label for="sel1">Select list:</label> -->
+      <select class="form-control" v-model="kmltype_sel" id="kmltype_sel">
+        <option disabled value='null'>Select a GNSS data model</option>
+        <option value='getvelocities'>Velocities</option>
+        <option value='getcoseismic'>Coseismic</option>
+        <option value='getpostseismic'>Postseismic</option>
+        <option value='getdisplacement'>Displacement</option>
+        <option value='getmodel'>Model</option>
+      </select>
 
-        <div v-if="kmltype_sel!=null">
-        <b-button style="margin-top: 10px; margin-bottom: 10px;" v-if="!geometryActive" id="sp_windowpicker" class="btn_blue" @click="gnssDrawRect()">
-         Draw an area on the map</b-button>
-        <b-button v-if="gnssLayers.length>0 || areaLayer!=null" class="btn_white" id="clearGnss" @click="clearGnss()"> 
-          Clear Layers</b-button>
+      <div v-if="kmltype_sel!=null">
+        <b-button style="margin-top: 10px; margin-bottom: 10px;" v-if="!geometryActive" id="sp_windowpicker"
+                  class="btn_blue" @click="gnssDrawRect()">
+          Draw an area on the map
+        </b-button>
+        <b-button v-if="gnssLayers.length>0 || areaLayer!=null" class="btn_white" id="clearGnss" @click="clearGnss()">
+          Clear Layers
+        </b-button>
         <br/>
 
-        <div v-if="geometryActive" >
+        <div v-if="geometryActive">
           <br/>
           <b-button class="btn_white" @click="drawListenerOff">
-            Unselect 'Draw an Area'</b-button>
+            Unselect 'Draw an Area'
+          </b-button>
           <br/>
         </div>
         <br>
         <span class="inputLabel">Center Latitude</span>
         <b-input-group>
-          <b-form-input v-model="gs_latitude"  name="gs_latitude"></b-form-input>
+          <b-form-input v-model="gs_latitude" name="gs_latitude"></b-form-input>
         </b-input-group>
 
         <span class="inputLabel">Center Longitude</span>
@@ -66,16 +71,17 @@
 
         <span class="inputLabel">Longitude Span</span>
         <b-input-group>
-          <b-form-input v-model="gs_width"  name="gs_width" placeholder="1 degree"></b-form-input>
+          <b-form-input v-model="gs_width" name="gs_width" placeholder="1 degree"></b-form-input>
         </b-input-group>
 
         <span class="inputLabel">Latitude Span</span>
-        <b-input-group >
+        <b-input-group>
           <b-form-input v-model="gs_height" placeholder="1 degree" name="gs_height"></b-form-input>
         </b-input-group>
 
         <span v-if="this.kmltype_sel === 'getcoseismic' || this.kmltype_sel === 'getpostseismic'" class="inputLabel">Epoch</span>
-        <div class="input-group" id="epoch_show" v-if="this.kmltype_sel === 'getcoseismic' || this.kmltype_sel === 'getpostseismic'">
+        <div class="input-group" id="epoch_show"
+             v-if="this.kmltype_sel === 'getcoseismic' || this.kmltype_sel === 'getpostseismic'">
           <b-input-group>
             <b-form-input v-model="gs_epoch" placeholder="YYYY-MM-DD" name="gs_epoch"></b-form-input>
           </b-input-group>
@@ -87,7 +93,7 @@
         </b-input-group>
 
         <span v-if="this.kmltype_sel === 'getdisplacement' || this.kmltype_sel === 'getmodel'" class="inputLabel">Epoch 2 </span>
-        <b-input-group  v-if="this.kmltype_sel === 'getdisplacement' || this.kmltype_sel === 'getmodel'">
+        <b-input-group v-if="this.kmltype_sel === 'getdisplacement' || this.kmltype_sel === 'getmodel'">
           <b-form-input v-model="gs_epoch2" placeholder="YYYY-MM-DD" name="gs_epoch2"></b-form-input>
         </b-input-group>
 
@@ -95,7 +101,9 @@
         <b-input-group>
           <b-form-input v-model="gs_refsite" placeholder="4-letter code" name="gs_refsite"></b-form-input>
           <b-input-group-append>
-            <b-button variant="outline-primary" href="https://sideshow.jpl.nasa.gov/post/tables/table2.html" target="_blank">Stations</b-button>
+            <b-button variant="outline-primary" href="https://sideshow.jpl.nasa.gov/post/tables/table2.html"
+                      target="_blank">Stations
+            </b-button>
           </b-input-group-append>
         </b-input-group>
 
@@ -130,22 +138,22 @@
         </b-input-group>
         <b-col class="miscOptions">
           <b-row class="checkbox" style="text-align: left" v-if="this.kmltype_sel === 'getdisplacement'">
-            <label class="checkbox" >
+            <label class="checkbox">
               <input v-model="gs_analysisCenter" name="analysisCenter" type="checkbox" id="gs_analysisCenter"/>
               Use NGL data
             </label>
           </b-row>
-          
-          
+
+
           <b-row class="checkbox" style="text-align: left">
-            <label class="checkbox" >
+            <label class="checkbox">
               <b-form-checkbox v-model="markerSize" name="vabs" type="checkbox" id="markerSize"/>
               Minimize Marker Size
             </label>
           </b-row>
           <b-row class="checkbox" style="text-align: left">
             <label class="checkbox">
-              <input v-model="gs_vabs" name="vabs" type="checkbox" id="gs_vabs" value= ""/>
+              <input v-model="gs_vabs" name="vabs" type="checkbox" id="gs_vabs" value=""/>
               Display absolute verticals
             </label>
           </b-row>
@@ -158,47 +166,48 @@
 
 
           <b-row>
-            <button  class="btn btn-success" id="gs_submit" name="submit" type="submit" v-on:click.prevent="runButtonClick()">        Run
+            <button class="btn btn-success" id="gs_submit" name="submit" type="submit"
+                    v-on:click.prevent="runButtonClick()"> Run
             </button>
           </b-row>
-          <br />
+          <br/>
           <b-row>
-            <div style="float: left; text-align: left"><strong>Data source: <br/><a href="https://sideshow.jpl.nasa.gov/post/series.html" target="_blank">GNSS Time Series</a></strong></div>
+            <div style="float: left; text-align: left"><strong>Data source: <br/><a
+                href="https://sideshow.jpl.nasa.gov/post/series.html" target="_blank">GNSS Time Series</a></strong>
+            </div>
           </b-row>
         </b-col>
 
-        <b-col >
+        <b-col>
         </b-col>
 
 
-
- 
-      <div v-if="activeGnssQuery" style="overflow: hidden">
+        <div v-if="activeGnssQuery" style="overflow: hidden">
+          <br/>
+          <b-spinner variant="success" label="Spinning"></b-spinner>
+        </div>
         <br/>
-        <b-spinner variant="success" label="Spinning"></b-spinner>
-      </div>
-      <br/>
       </div>
 
 
     </div>
 
     <!-- info  popup -->
-    <b-modal 
-    v-model="gnssInfo"
-            title="GNSS">
-            <p class="my-4">
-              Global Navigation Satellite System (GNSS) is any satellite 
-              constellation which provides positioning, navigation, and 
-              timing (PNT) services on a global or regional basis (Other 
-              Global Navigation Satellite Systems (GNSS), 2020). One of the 
-              systems GNSS includes is the United States-owned Global Positioning 
-              System (GPS). 
-            </p>
+    <b-modal
+        v-model="gnssInfo"
+        title="GNSS">
+      <p class="my-4">
+        Global Navigation Satellite System (GNSS) is any satellite
+        constellation which provides positioning, navigation, and
+        timing (PNT) services on a global or regional basis (Other
+        Global Navigation Satellite Systems (GNSS), 2020). One of the
+        systems GNSS includes is the United States-owned Global Positioning
+        System (GPS).
+      </p>
 
-            <div slot="modal-footer" class="w-100">
-            </div>
-          </b-modal>
+      <div slot="modal-footer" class="w-100">
+      </div>
+    </b-modal>
 
   </div>
 
@@ -209,7 +218,7 @@
 
 import {bus} from '@/main'
 import axios from 'axios'
-import { mapFields } from 'vuex-map-fields';
+import {mapFields} from 'vuex-map-fields';
 import L from "leaflet";
 
 export default {
@@ -249,60 +258,63 @@ export default {
       'gnss.layersActive',
       'gnss.activeGnssQuery',
       'gnss.geometryActive',
-        'gnss.gnssLayers',
+      'gnss.gnssLayers',
 
-        'map.drawControl',
-        'map.globalMap',
-        'map.layers',
+      'map.drawControl',
+      'map.globalMap',
+      'map.layers',
     ])
 
   },
   mounted() {
     bus.$on('gnssDrawQuery', (maxLat, minLon, minLat, maxLon, centerLat, centerLng) =>
         this.setRect(maxLat, minLon, minLat, maxLon, centerLat, centerLng));
-    this.kmltype_sel=null;
+    this.kmltype_sel = null;
   },
 
   methods: {
-    showHideLayers(active, layer){
+    showHideLayers(active, layer) {
       let name = layer.pre + layer.type;
-        if(active){
-          this.globalMap.addLayer(this.layers[name])
-        }else {
-          this.globalMap.removeLayer(this.layers[name]);
-        }
+      if (active) {
+        this.globalMap.addLayer(this.layers[name])
+      } else {
+        this.globalMap.removeLayer(this.layers[name]);
+      }
 
-      },
-    runButtonClick(){
+    },
+    runButtonClick() {
       let vm = this;
-      if(vm.areaLayer!=null){
+      if (vm.areaLayer != null) {
         vm.globalMap.removeLayer(vm.areaLayer);
-        vm.areaLayer=null;
+        vm.areaLayer = null;
       }
       this.rungpsservice();
     },
-    rungpsservice(){
+    rungpsservice() {
       this.activeGnssQuery = true;
       var vm = this;
       var fileNameH, fileNameV, fileNameT, folder, props;
       //var markerSize = this.markerSize;
       var verticalUrl, horizontalUrl, tableUrl;
       var prefix = this.gs_outputprefix;
-      if(this.kmltype_sel === ''){
+      if (this.kmltype_sel === '') {
         alert("Please select as least one plot!");
-      }
-      else {
+      } else {
 
-        for(var i = 0;i<this.gnssLayers.length;i++){
+        for (var i = 0; i < this.gnssLayers.length; i++) {
           var splitPrefix = this.gnssLayers[i].name.split('_')[0];
-          if(splitPrefix === this.gs_outputprefix){
+          if (splitPrefix === this.gs_outputprefix) {
             continue;
             //alert('There is already an existing query with that name, please rename and resubmit');
             //return;
           }
         }
         // this.layerCheckbox = true;
-        if (this.gs_analysisCenter == true) {this.gs_analysisCenter = "NGL";} else { this.gs_analysisCenter = "";}
+        if (this.gs_analysisCenter == true) {
+          this.gs_analysisCenter = "NGL";
+        } else {
+          this.gs_analysisCenter = "";
+        }
         const baseURI = '/geogateway_django_app/gps_service'
         //request JSON dict of GPS_service details with query params from form
         axios.get(baseURI, {
@@ -326,7 +338,7 @@ export default {
             "dwin2": this.gs_dwin2,
             "prefix": this.gs_outputprefix,
             //need default false value?
-            "mon":this.markerSize,
+            "mon": this.markerSize,
             "eon": this.gs_eon,
             "vabs": this.gs_vabs,
             "analysisCenter": this.gs_analysisCenter,
@@ -342,26 +354,28 @@ export default {
                 alert("Somthing wrong, please check input paramters!");
                 return;
               }
+
               function getExtension(f) {
                 var parts = f.split('_');
                 return parts[parts.length - 1];
               }
-              for(var i = 0;i < 3;i++){
+
+              for (var i = 0; i < 3; i++) {
                 var ext = getExtension(props.urls[i]);
-                if(ext == 'vertical.kml'){
+                if (ext == 'vertical.kml') {
                   verticalUrl = props.urls[i];
                   fileNameV = props.results[i];
-                }else if(ext == 'horizontal.kml'){
+                } else if (ext == 'horizontal.kml') {
                   horizontalUrl = props.urls[i];
                   fileNameH = props.results[i];
-                }else if(ext == 'table.txt'){
+                } else if (ext == 'table.txt') {
                   tableUrl = props.urls[i];
                   fileNameT = props.results[i];
                 }
               }
 
               folder = props.folder;
-              prefix = (1 + Math.floor(vm.gnssLayers.length/3)).toString() + prefix;
+              prefix = (1 + Math.floor(vm.gnssLayers.length / 3)).toString() + prefix;
               vm.gnssLayers.push({
                 pre: prefix,
                 name: fileNameT,
@@ -438,13 +452,13 @@ export default {
 
       this.drawListener('gnss');
     },
-    gnssDrawRect(){
+    gnssDrawRect() {
       this.geometryActive = true;
       let vm = this;
       vm.rectDraw = new L.Draw.Rectangle(vm.globalMap, vm.drawControl.options.rectangle);
       vm.rectDraw.enable();
       vm.globalMap.on('draw:created', function (e) {
-        if(vm.areaLayer!=null){
+        if (vm.areaLayer != null) {
           vm.globalMap.removeLayer(vm.areaLayer)
         }
         var type = e.layerType;
@@ -457,40 +471,41 @@ export default {
           vm.maxLon = layer.getLatLngs()[0][2].lng;
           vm.minLat = layer.getLatLngs()[0][3].lat;
           vm.minLon = layer.getLatLngs()[0][0].lng;
-          vm.areaLayer=layer;
+          vm.areaLayer = layer;
           vm.rectDraw = null;
           bus.$emit('gnssDrawQuery', vm.maxLat, vm.minLon, vm.minLat, vm.maxLon, vm.centerLat, vm.centerLng)
           vm.geometryActive = false;
-        }});
+        }
+      });
 
     },
-    drawListenerOff(){
+    drawListenerOff() {
       this.geometryActive = false;
 
       this.rectDraw.disable();
     },
-    clearGnss(){
+    clearGnss() {
       let vm = this;
-      if(vm.areaLayer!=null){
+      if (vm.areaLayer != null) {
         vm.globalMap.removeLayer(vm.areaLayer);
-        vm.areaLayer=null;
+        vm.areaLayer = null;
       }
       this.layersActive = false;
-      for(var i = 0; i < this.gnssLayers.length; i++){
+      for (var i = 0; i < this.gnssLayers.length; i++) {
         let curr = this.gnssLayers[i];
-        if(curr.type !== 'table.txt') {
+        if (curr.type !== 'table.txt') {
           let name = curr.pre + curr.type;
           this.globalMap.removeLayer(this.layers[name]);
         }
       }
-      
-      this.gs_latitude =null;
+
+      this.gs_latitude = null;
       this.gs_longitude = null;
       this.gs_width = null;
       this.gs_height = null;
       this.gnssLayers = [];
     },
-    setRect(maxLat, minLon, minLat, maxLon, centerLat, centerLng){
+    setRect(maxLat, minLon, minLat, maxLon, centerLat, centerLng) {
       this.gs_latitude = centerLat.toFixed(5);
       this.gs_longitude = centerLng.toFixed(5);
       this.gs_height = Math.abs(maxLat - minLat).toFixed(5);
@@ -506,6 +521,7 @@ export default {
 .input-group {
   width: 100%;
 }
+
 strong {
   color: #343a40;
 }
@@ -526,7 +542,7 @@ strong {
   background-color: #bad7ff;
   text-align: left;
   margin-right: auto;
-  padding:5px;
+  padding: 5px;
   position: absolute;
 }
 
